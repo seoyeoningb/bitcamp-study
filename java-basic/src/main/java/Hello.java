2f5a0d1c2cfa967158a3273fc64fe491e51c5ddb
seoyeoningb <baesy90@naver.com>
2021-06-30 17:39:44 +0900

    ok

--- java-basic/src/main/java/Hello.java
+++ java-basic/src/main/java/Hello.java
@@ -4,7 +4,8 @@ class Hello {
     // -프로그램 실행 시작점        |
     // - 'entry point'라 부름       |
     // ------------------------------
-    public static void main(string[] args) {
-        system.out.println("Hello.world");
+    public static void main(String[] args) {
+        System.out.println("Hello.world");
+        System.out.println("안녕하세요!");
     }
 }
