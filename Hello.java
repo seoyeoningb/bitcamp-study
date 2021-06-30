ac4cd8c7a14f4c7978750bc5888096a6b93074c1
seoyeoningb <baesy90@naver.com>
2021-06-30 15:08:43 +0900

    ok

---
+++ Hello.java
@@ -0,0 +1,10 @@
+class Hello {
+    // ------------------------------
+    // main()                       |
+    // -프로그램 실행 시작점        |
+    // - 'entry point'라 부름       |
+    // ------------------------------
+    public static void main(string[] args) {
+        system.out.println("Hello.world");
+    }
+}
