df07533476d9034afa72406972893033bb6abb71
seoyeoningb <baesy90@naver.com>
2021-07-05 18:29:39 +0900

    .

---
+++ study-project/app/src/main/java/com/eomcs/pms/App3.java
@@ -0,0 +1,14 @@
+package com.eomcs.pms;
+
+public class App3 {
+
+  public static void main(String[] args) {
+    System.out.printf("[프로젝트]");
+    System.out.printf("번호: 1201");
+    System.out.printf("프로젝트명 : 미니프로젝트 관리 시스템 개발"); 
+    System.out.printf("내용 : 소규모 팀을 위한 프로젝트 관리 시스템을 개발한다."); 
+    System.out.printf("20210101"); 
+    System.out.printf("20211231"); 
+    System.out.printf("만든이: 홍길동");
+  }
+}
